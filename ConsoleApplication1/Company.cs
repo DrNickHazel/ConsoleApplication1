b966762521a01811f580067d461ed04d5cba1d18
Git Hazel <hazelgit@outlook.com>
2012-08-20 00:22:09 +0100

    Refactored Company and Employee classes
    Refactored Company and Employee classes into separate class files.

---
+++ ConsoleApplication1/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Company
+    {
+        public string Name { get; set; }
+
+        public Company() { Name = "Unknown"; }
+
+        public Employee this[int index]
+        {
+            get { return employees[index]; }
+        }
+
+        public int Count { get { return employees.Count; } }
+
+        public void Add(Employee e) { employees.Add(e); }
+
+        private List<Employee> employees = new List<Employee>();
+    }
+}
---
+++ ConsoleApplication1/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Employee()
+        {
+            Name = "Unknown";
+            Age = -1;
+        }
+        public Employee(string name) : this() { Name = name; }
+        public Employee(string name, int age = -1) : this(name) { Age = age; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Employee(Name: ");
+            sb.Append(Name);
+            sb.Append(", Age: ");
+            sb.Append(Age);
+            sb.Append(")");
+
+            return sb.ToString();
+        }
+    }
+}
