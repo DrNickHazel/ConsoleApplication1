c5612dc7260a43ebe03405b7008a6c143411f350
Dr Nicholas M. Hazel <aroi-thai@outlook.com>
2026-09-17 20:49:46 +0700

    Updated the build target to the latest versoin of .NET Framework.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
@@ -13,7 +9,7 @@ namespace ConsoleApplication1
             Company c = new Company { Name = "Hazelcom Ltd" };
 
             c.Add(new Employee("Nicholas"));						// Constructs Employee with default Age value
-            c.Add(new Employee { Name = "Tom" });
+            c.Add(new Employee { Name = "Tom" });					// Constructs Employee with default Age value
             c.Add(new Employee { Name = "Dick", Age = 20 });
             c.Add(new Employee { Name = "Harry", Age = 30 });
             c.Add(new Employee());
